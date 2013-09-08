6351b5f4b4ca53512d0be0ba64db9cfdba35c603
Jared <jared_de_la_cruz@yahoo.com>
2013-09-07 20:02:03 -0700

    improved ui

--- CogswellRegistrar/CogswellRegistrar/Form1.h
+++ CogswellRegistrar/CogswellRegistrar/Form1.h
@@ -14,7 +14,7 @@ namespace CogswellRegistrar {
 	/// </summary>
 	public ref class Form1 : public System::Windows::Forms::Form
 	{
-	public:
+		public:
 		Form1(void)
 		{
 			InitializeComponent();
@@ -22,8 +22,7 @@ namespace CogswellRegistrar {
 			//TODO: Add the constructor code here
 			//
 		}
-
-	protected:
+		protected:
 		/// <summary>
 		/// Clean up any resources being used.
 		/// </summary>
@@ -34,22 +33,25 @@ namespace CogswellRegistrar {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::GroupBox^  group_settings;
-	protected: 
-	private: System::Windows::Forms::Button^  btn_audit;
-	private: System::Windows::Forms::Button^  btn_master;
-	private: System::Windows::Forms::Button^  btn_search;
-	private: System::Windows::Forms::TextBox^  input_search;
-	private: System::Windows::Forms::GroupBox^  group_table;
-
-	protected: 
+		private: System::Windows::Forms::GroupBox^  group_settings;
+		private: System::Windows::Forms::Button^  btn_audit;
+		private: System::Windows::Forms::Button^  btn_master;
+		private: System::Windows::Forms::Button^  btn_search;
+		private: System::Windows::Forms::TextBox^  input_search;
+		private: System::Windows::Forms::GroupBox^  group_table;
+		public: OpenFileDialog^ audit_file;
+		public: OpenFileDialog^ master_file;
+	private: System::Windows::Forms::Label^  label_master;
+	public: 
+	private: System::Windows::Forms::Label^  label_audit;
+	private: System::Windows::Forms::Button^  btn_run;
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
 		System::ComponentModel::Container ^components;
 
-#pragma region Windows Form Designer generated code
+		#pragma region Windows Form Designer generated code
 		/// <summary>
 		/// Required method for Designer support - do not modify
 		/// the contents of this method with the code editor.
@@ -58,31 +60,56 @@ namespace CogswellRegistrar {
 		{
 			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(Form1::typeid));
 			this->group_settings = (gcnew System::Windows::Forms::GroupBox());
+			this->label_master = (gcnew System::Windows::Forms::Label());
+			this->label_audit = (gcnew System::Windows::Forms::Label());
 			this->btn_search = (gcnew System::Windows::Forms::Button());
 			this->input_search = (gcnew System::Windows::Forms::TextBox());
 			this->btn_master = (gcnew System::Windows::Forms::Button());
 			this->btn_audit = (gcnew System::Windows::Forms::Button());
 			this->group_table = (gcnew System::Windows::Forms::GroupBox());
+			this->btn_run = (gcnew System::Windows::Forms::Button());
 			this->group_settings->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// group_settings
 			// 
-			this->group_settings->AutoSize = true;
-			this->group_settings->AutoSizeMode = System::Windows::Forms::AutoSizeMode::GrowAndShrink;
+			this->group_settings->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left) 
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->group_settings->Controls->Add(this->btn_run);
+			this->group_settings->Controls->Add(this->label_master);
+			this->group_settings->Controls->Add(this->label_audit);
 			this->group_settings->Controls->Add(this->btn_search);
 			this->group_settings->Controls->Add(this->input_search);
 			this->group_settings->Controls->Add(this->btn_master);
 			this->group_settings->Controls->Add(this->btn_audit);
 			this->group_settings->Location = System::Drawing::Point(12, 12);
 			this->group_settings->Name = L"group_settings";
-			this->group_settings->Size = System::Drawing::Size(600, 90);
+			this->group_settings->Size = System::Drawing::Size(600, 82);
 			this->group_settings->TabIndex = 0;
 			this->group_settings->TabStop = false;
 			this->group_settings->Text = L"Settings";
 			// 
+			// label_master
+			// 
+			this->label_master->AutoSize = true;
+			this->label_master->Location = System::Drawing::Point(87, 53);
+			this->label_master->Name = L"label_master";
+			this->label_master->Size = System::Drawing::Size(62, 13);
+			this->label_master->TabIndex = 5;
+			this->label_master->Text = L"Select file...";
+			// 
+			// label_audit
+			// 
+			this->label_audit->AutoSize = true;
+			this->label_audit->Location = System::Drawing::Point(87, 24);
+			this->label_audit->Name = L"label_audit";
+			this->label_audit->Size = System::Drawing::Size(62, 13);
+			this->label_audit->TabIndex = 4;
+			this->label_audit->Text = L"Select file...";
+			// 
 			// btn_search
 			// 
+			this->btn_search->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
 			this->btn_search->Location = System::Drawing::Point(519, 19);
 			this->btn_search->Name = L"btn_search";
 			this->btn_search->Size = System::Drawing::Size(75, 23);
@@ -92,6 +119,7 @@ namespace CogswellRegistrar {
 			// 
 			// input_search
 			// 
+			this->input_search->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
 			this->input_search->Location = System::Drawing::Point(345, 21);
 			this->input_search->Name = L"input_search";
 			this->input_search->Size = System::Drawing::Size(168, 20);
@@ -105,6 +133,7 @@ namespace CogswellRegistrar {
 			this->btn_master->TabIndex = 1;
 			this->btn_master->Text = L"Master List";
 			this->btn_master->UseVisualStyleBackColor = true;
+			this->btn_master->Click += gcnew System::EventHandler(this, &Form1::btn_master_Click);
 			// 
 			// btn_audit
 			// 
@@ -114,15 +143,29 @@ namespace CogswellRegistrar {
 			this->btn_audit->TabIndex = 0;
 			this->btn_audit->Text = L"Audit List";
 			this->btn_audit->UseVisualStyleBackColor = true;
+			this->btn_audit->Click += gcnew System::EventHandler(this, &Form1::btn_audit_Click);
 			// 
 			// group_table
 			// 
-			this->group_table->Location = System::Drawing::Point(12, 109);
+			this->group_table->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom) 
+				| System::Windows::Forms::AnchorStyles::Left) 
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->group_table->Location = System::Drawing::Point(12, 100);
 			this->group_table->Name = L"group_table";
-			this->group_table->Size = System::Drawing::Size(600, 321);
+			this->group_table->Size = System::Drawing::Size(600, 330);
 			this->group_table->TabIndex = 1;
 			this->group_table->TabStop = false;
 			this->group_table->Text = L"Table";
+			// 
+			// btn_run
+			// 
+			this->btn_run->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
+			this->btn_run->Location = System::Drawing::Point(519, 48);
+			this->btn_run->Name = L"btn_run";
+			this->btn_run->Size = System::Drawing::Size(75, 23);
+			this->btn_run->TabIndex = 6;
+			this->btn_run->Text = L"Process";
+			this->btn_run->UseVisualStyleBackColor = true;
 			// 
 			// Form1
 			// 
@@ -138,13 +181,31 @@ namespace CogswellRegistrar {
 			this->group_settings->ResumeLayout(false);
 			this->group_settings->PerformLayout();
 			this->ResumeLayout(false);
-			this->PerformLayout();
 
 		}
-#pragma endregion
-	private: System::Void Form1_Load(System::Object^  sender, System::EventArgs^  e) {
-			 }
+		#pragma endregion
+		private: System::Void Form1_Load(System::Object^  sender, System::EventArgs^  e) {
+		}
 
+		private: System::Void btn_audit_Click(System::Object^  sender, System::EventArgs^  e) {
+			audit_file = gcnew OpenFileDialog();
+			audit_file->Filter = "CSV Files|*.csv|All Files|*.*";
+			if(audit_file->ShowDialog() != System::Windows::Forms::DialogResult::OK)
+			{
+				return;
+			}
+			//MessageBox::Show(audit_file->FileName);
+			this->label_audit->Text = audit_file->FileName;
+		}
+		private: System::Void btn_master_Click(System::Object^  sender, System::EventArgs^  e) {
+			master_file = gcnew OpenFileDialog();
+			master_file->Filter = "CSV Files|*.csv|All Files|*.*";
+			if(master_file->ShowDialog() != System::Windows::Forms::DialogResult::OK)
+			{
+				return;
+			}
+			//MessageBox::Show(master_file->FileName);
+			this->label_master->Text = master_file->FileName;
+		}
 };
 }
-
